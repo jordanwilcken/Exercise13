6692bb0d000438d06d425253b4b8a1fe26dbb282
jordanwilcken@github.com <jordanwilcken@gmail.com>
2015-01-28 08:19:31 -0700

    routing now correct for Azure

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ var
 app.configure( function () {
   app.use( express.bodyParser() );
   app.use( express.methodOverride() );
-  app.use( express.static( __dirname + '/public' ) );
+  app.use( '/pacmanai', express.static( __dirname + '/public') );
   app.use( app.router );
 });
 
